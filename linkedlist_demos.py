6daa5d46147ac3e41b11e9f6580ec698d135158e
A C Gould <gould.ashleyc@gmail.com>
2016-12-05 20:52:45 -0800

    added LinkedList reverse with tests

--- linkedlist_demos.py
+++ linkedlist_demos.py
@@ -4,20 +4,23 @@
 class Node:
     """ A simple node from which to create a singly linked list"""
     def __init__(self, value=None, next=None):
+        """An initalization with default values and next of None. If only 
+        one input is passed, it will be considered the value.
+        """
         self.next = next
         self.value = value
 
     def get_value(self):
+        """Returns the Node's value"""
         return self.value
 
     def get_next(self):
+        """Returns Node's next"""
         return self.next
 
     def set_next(self, new_next):
+        """Changes Node's next"""
         self.next = new_next
-
-    def print_node(self):
-        print self.value
 
 class LinkedList:
     """A simple class to hold the head of the linked list"""
@@ -25,12 +28,17 @@ class LinkedList:
         self.head = head
 
     def get_head(self):
+        """Returns the node at the head of the LinkedList. """
         return self.head
 
     def set_head(self, new_head):
+        """Sets the head of the LinkedList to new_head. WARNING: current list
+        nodes will be lost.
+        """
         self.head = new_head
 
     def return_list(self):
+        """Returns a standard list with LinkedList's values for debugging."""
         L = []
         current = self.head
         while current != None:
@@ -38,25 +46,42 @@ class LinkedList:
             current = current.get_next()
         return L
 
+    def append_to_front(self, new_node):
+        """Appends new_node to the front of the LinkedList. O(1)"""
+        old_head = self.head
+        self.head = new_node
+        new_node.set_next(old_head)
+
+    def append_to_end(self, new_node):
+        """Appends new_node to the front of the LinkedList. O(n)"""
+        if self.head == None:
+            self.head = new_node
+        else:
+            previous = None
+            current = self.head
+            # step through to get to the end
+            while current != None:
+                previous = current
+                current = current.get_next()
+            # attach new node to the end
+            previous.set_next(new_node)
 
 
-def reverse(linkedlist):
-    # first get the head of the list
-    current = linkedlist.get_head()
-    # there is no previous yet
-    previous = None
-    # call recursive function to iteratively reverse list and set new head
-    linkedlist.set_head(node_reverse(previous, current))
+    def reverse(self):
+        """Reverse the LinkedList"""
+        # call recursive function to iteratively reverse list and set new head
+        self.head = self.node_reverse(None, self.head)
 
-def node_reverse(previous, current):
-    # base case: no more nodes
-    if current == None:
-        return previous
-    else:
-        # first, keep track of the current next
-        next = current.get_next()
-        # swap the direction of the current node
-        current.set_next(previous)
-        # step forward so that previous becomes what was current 
-        # and current becomes what was next
-        node_reverse(current, next)
+    def node_reverse(self, previous, current):
+        """A recursive function to perform the reversal of the LinkedList"""
+        # base case: no more nodes
+        if current == None:
+            return previous
+        else:
+            # first, keep track of the current next
+            next = current.get_next()
+            # swap the direction of the current node
+            current.set_next(previous)
+            # step forward so that previous becomes what was current 
+            # and current becomes what was next
+            return self.node_reverse(current, next)
--- test_linkedlist_demos.py
+++ test_linkedlist_demos.py
@@ -56,6 +56,76 @@ class TestLinkedList:
         assert LL.get_head().get_value() == n.get_value()
         assert LL.get_head().get_value() == 1
 
+    def test_append_to_list(self):
+        LLa = LinkedList()
+        LLb = LinkedList() 
+        n0 = Node(0)
+        n1 = Node(1)
+        # both LinkedLists are currently empty
+        assert LLa.get_head() == None
+        assert LLb.get_head() == None
+        # append to empty list
+        LLa.append_to_front(n0)
+        LLb.append_to_end(n0)
+        assert LLa.get_head().get_value() == 0
+        assert LLb.get_head().get_value() == 0
+        # append to non-empty list
+        LLa.append_to_front(n1)
+        LLb.append_to_end(n1)
+        assert LLa.get_head().get_value() == 1
+        assert LLb.get_head().get_value() == 0
+        assert LLa.get_head().get_next().get_value() == 0
+        assert LLb.get_head().get_next().get_value() == 1
+
+class TestReverse:
+
+    def test_empty(self):
+        LL = LinkedList()
+        assert LL.get_head() == None
+        LL.reverse()
+        assert LL.get_head() == None
+
+    def test_one(self):
+        n = Node(1)
+        LL = LinkedList(n)
+        assert LL.get_head() == n
+        LL.reverse()
+        assert LL.get_head() == n
+
+    def test_two(self):
+        n0 = Node(0)
+        n1 = Node(1)
+        LL = LinkedList()
+        LL.append_to_end(n0)
+        LL.append_to_end(n1)
+        assert LL.get_head() == n0
+        assert n0.get_next() == n1
+        LL.reverse()
+        assert LL.get_head() == n1
+        assert n1.get_next() == n0
+
+    def long_list(self, length):
+        next = None
+        for value in range(length):
+            next = Node(value, next)
+        return next
+
+    def test_long(self):
+        LL = LinkedList(self.long_list(10))
+        assert LL.get_head().get_value() == 9
+        LL.reverse()
+        assert LL.get_head().get_value() == 0
+        assert LL.return_list() == range(10)
+
+    def test_longer(self):
+        LL = LinkedList(self.long_list(100))
+        assert LL.get_head().get_value() == 99
+        LL.reverse()
+        assert LL.get_head().get_value() == 0
+        assert LL.return_list() == range(100)
+
+
+
 
 
 
